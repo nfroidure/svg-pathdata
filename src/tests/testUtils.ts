0cedeb16637ac1addcde2514cfab31ce23976062
armano <armano2@users.noreply.github.com>
2025-02-28 20:12:11 +0100

    chore: remove yerror from dependencies and use jest.expect

--- src/tests/testUtils.ts
+++ src/tests/testUtils.ts
@@ -1,16 +1,7 @@
 import { expect } from '@jest/globals';
-import { YError } from 'yerror';
 
-export function assertThrows<T>(
-  fn: () => void,
-  type: T,
-  message: string,
-) {
-  try {
-    fn();
-    throw new YError('E_UNEXPECTED_SUCCESS');
-  } catch (err) {
-    expect(err).toBeInstanceOf(type);
-    expect((err as Error).message).toEqual(message);
-  }
+export function assertThrows<T>(fn: () => void, type: T, message: string) {
+  const expRes = expect(fn);
+  expRes.toThrowError(message);
+  expRes.toThrow(type);
 }
