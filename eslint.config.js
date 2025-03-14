b80636d63bcb889a0498aa72ee5f838aa942b4a7
armano <armano2@users.noreply.github.com>
2025-03-14 19:30:22 +0100

    chore: update eslint-plugin-vitest import and dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginVitest from 'eslint-plugin-vitest';
+import eslintPluginVitest from '@vitest/eslint-plugin';
 
 export default tseslint.config(
   eslint.configs.recommended,
